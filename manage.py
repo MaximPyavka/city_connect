d37c7d201165581d5e41cc1728f233b1ff21761e
MaximPyavka <maxpyavka@gmail.com>
2017-05-05 22:00:24 +0300

    Added:  Manager Command --> create_admin, file --> modules/modules, class User

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
+from city_connect.models.models import User
+from datetime import datetime
 from city_connect.app import app, db
 
 manager = Manager(app)
@@ -8,6 +9,18 @@ migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def create_admin():
+    """Creates the admin user"""
+    db.session.add(User(
+        email = "ad@min.com",
+        password = "admin",
+        admin = True,
+        confirmed = True,
+        confirmed_on = datetime.now())
+    )
+    db.session.commit()
 
 @manager.command
 def create_db():
